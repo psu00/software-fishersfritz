51331190e5b041383c360c8d22f99c8ff8a4a54d
Phillip <sucic.phillip@gmail.com>
2025-01-05 21:30:28 +0100

    feat: activate cors in app.py and register module catch.py

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from catch import catch_blueprint
-
 app = Flask(__name__)
-app.register_blueprint(catch_blueprint, url_prefix='/api')
-
+CORS(app)  # CORS aktivieren
+# Blueprint registrieren
+app.register_blueprint(catch_blueprint)
 @app.route('/')
 def home():
     return "Welcome to FisherFritz API!"
-
 if __name__ == '__main__':
     app.run(debug=True)
