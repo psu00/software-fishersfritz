89223848e38b967ccc3fa991a48c55d224191c0e
DG15 <daniel.gunkel@edu.fh-wien.ac.at>
2025-01-17 17:48:07 +0100

    Prefilling the setup_db.py
    Vorbeladung der Datenbank mit Musterbeispielen.

--- backend/setup_db.py
+++ backend/setup_db.py
@@ -28,6 +28,18 @@ CREATE TABLE IF NOT EXISTS fish (
 )
 ''')
 
+catch_list = [
+    # Perschling, Angelgebiet
+    ("Rotauge", 48.2700, 15.8000, 0.8, "2024-11-11"),  
+    ("Karpfen", 48.2700, 15.8000, 3.8, "2024-11-11"),
+    # Krems, Angelgebiet
+    ("Wels", 48.4100, 15.6000, 5.0, "2024-12-12"),
+    # Tulln, Angelgebiet
+    ("Zander", 48.3300, 16.0700, 4.2, "2024-12-30"),  
+    # Krems, Angelgebiet
+    ("Hecht", 48.4100, 15.6000, 2.5, "2025-01-12") 
+]
+
 # Beispiel-Daten für die Fischarten hinzufügen
 fish_list = [
     ("Rotauge", True, None, None, 15),  # Keine spezifische Schonzeit
@@ -43,6 +55,11 @@ fish_list = [
     ("Seeforelle", True, "10-01", "02-28", 60)
 ]
 
+# Daten nur einfügen, wenn die Tabelle leer ist
+cursor.execute('SELECT COUNT(*) FROM catches')
+if cursor.fetchone()[0] == 0:
+    cursor.executemany('INSERT INTO catches (fish_name, latitude, longitude, weight, date) VALUES (?, ?, ?, ?, ?)', catch_list)
+    print("Die Fänge wurden in die Datenbank eingefügt.")
 
 # Daten nur einfügen, wenn die Tabelle leer ist
 cursor.execute('SELECT COUNT(*) FROM fish')
